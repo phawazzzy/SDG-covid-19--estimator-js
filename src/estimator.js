eb4b2a223693b45222df5a478e951b65054d2751
kareem fawas <phawazzzy@gmail.com>
2020-04-14 15:56:08 +0100

    done

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,6 @@
 /* eslint-disable max-len */
 /* eslint-disable consistent-return */
 const covid19ImpactEstimator = (data) => {
-  // {
-  //   region: {
-  //   name: "Africa",
-  //   avgAge: 19.7,
-  //   avgDailyIncomeInUSD: 5,
-  //   avgDailyIncomePopulation: 0.71
-  //   },
-  //   periodType: "days",
-  //   timeToElapse: 58,
-  //   reportedCases: 674,
-  //   population: 66622705,
-  //   totalHospitalBeds: 1380614
-  //   }
-
   const {
     reportedCases,
     timeToElapse,
